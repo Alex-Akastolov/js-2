5f3f6e130ae22cd980ad10c35c49aaf53d30ac63
Oleksii Akastolov <akastolov1@gmail.com>
2022-06-12 17:45:49 +0300

    fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -248,21 +248,21 @@ console.log('------------')
 
 
 // Блок тестирования:
- console.log(1, ageClassification(-1) === null);
- console.log(2, ageClassification(1) === 'детский возраст');
- console.log(3, ageClassification(24) === 'детский возраст');
- console.log(4, ageClassification(24.01) === 'молодой возраст');
- console.log(5, ageClassification(44) === 'молодой возраст');
- console.log(6, ageClassification(44.01) === 'средний возраст');
- console.log(7, ageClassification(65) === 'средний возраст');
- console.log(8, ageClassification(65.01) === 'пожилой возраст');
- console.log(9, ageClassification(75) === 'пожилой возраст');
- console.log(10, ageClassification(75.01) === 'старческий возраст');
- console.log(11, ageClassification(90) === 'старческий возраст');
- console.log(12, ageClassification(90.01) === 'долгожители');
- console.log(13, ageClassification(122) === 'долгожители');
- console.log(14, ageClassification(122.01) === null);
- console.log(15, ageClassification(150) === null);
+//  console.log(1, ageClassification(-1) === null);
+//  console.log(2, ageClassification(1) === 'детский возраст');
+//  console.log(3, ageClassification(24) === 'детский возраст');
+//  console.log(4, ageClassification(24.01) === 'молодой возраст');
+//  console.log(5, ageClassification(44) === 'молодой возраст');
+//  console.log(6, ageClassification(44.01) === 'средний возраст');
+//  console.log(7, ageClassification(65) === 'средний возраст');
+//  console.log(8, ageClassification(65.01) === 'пожилой возраст');
+//  console.log(9, ageClassification(75) === 'пожилой возраст');
+//  console.log(10, ageClassification(75.01) === 'старческий возраст');
+//  console.log(11, ageClassification(90) === 'старческий возраст');
+//  console.log(12, ageClassification(90.01) === 'долгожители');
+//  console.log(13, ageClassification(122) === 'долгожители');
+//  console.log(14, ageClassification(122.01) === null);
+//  console.log(15, ageClassification(150) === null);
  
 /*
  * #7
